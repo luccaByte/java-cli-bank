b64c90a604b73a0e78b942e541953becdc665aeb
lucas <lucaston2323@gmail.com>
2024-09-19 16:28:22 -0300

    feat: menu mais interativo no cli

--- src/com/java/bank/Main.java
+++ src/com/java/bank/Main.java
@@ -17,38 +17,67 @@ public class Main {
         """);
         conta.setNome(sc.nextLine());
 
-        System.out.println("*********************************");
-        System.out.println("\nNome: " + conta.getNome());
-        System.out.println("Tipo de conta: ");
-        System.out.println("Saldo: " + conta.getSaldo());
-        System.out.println("\n*********************************");
-
         System.out.println("""
-        \n************* MENU **************
-        
-        1 - Consultar saldos
-        2 - Receber valor
-        3 - Transferir valor
-        4 - Sair
-        
-        Digite a opção desejada:
+        Escolha o tipo de conta:
+        1 - Conta Corrente
+        2 - Conta Poupança
         """);
-        input = sc.nextInt();
+        int tipoConta = sc.nextInt();
 
-        switch (input) {
-            case 1:
-                System.out.println("funcionou 1");
-                break;
-            case 2:
-                System.out.println("funcionou 2");
-                break;
-            case 3:
-                System.out.println("funcionou 3");
-                break;
-            case 4:
-                System.out.println("funcionou 4");
-                break;
+        if (tipoConta == 1) {
+            conta.setConta("Conta Corrente");
+        } else if (tipoConta == 2) {
+            conta.setConta("Conta Poupança");
+        } else {
+            System.out.println("Opção inválida");
         }
+
+        System.out.println("**************************************");
+        System.out.printf("""
+        Dados do Cliente
+        
+        Nome:                %s
+        Tipo de conta:       %s
+        Saldo:            R$ %.2f     \s
+        %n""", conta.getNome(), conta.getConta(), conta.getSaldo());
+        System.out.println("**************************************");
+
+        do {
+            System.out.println("""
+            \n**************** MENU ****************
+            
+            1 - Consultar saldos
+            2 - Receber valor
+            3 - Transferir valor
+            4 - Sair
+            
+            Digite a opção desejada:
+            """);
+            input = sc.nextInt();
+
+            switch (input) {
+                case 1:
+                    System.out.printf("\nSaldo atual:         R$ %.2f\n",conta.getSaldo());
+                    break;
+                case 2:
+                    System.out.println("\nInforme o valor a receber:");
+                    double valorDeposito = sc.nextDouble();
+                    conta.deposito(valorDeposito);
+                    System.out.printf("\nSaldo atualizado: R$ %.2f\n", conta.getSaldo());
+                    break;
+                case 3:
+                    System.out.println("\nInforme o valor a retirar:");
+                    double valorSaque = sc.nextDouble();
+                    conta.saque(valorSaque);
+                    System.out.printf("\nSaldo atualizado: R$ %.2f\n", conta.getSaldo());
+                    break;
+                case 4:
+                    System.out.println("\nSaindo. . .");
+                    break;
+                default:
+                    System.out.println("\nOpção inválida, tente novamente.");
+            }
+        } while (input != 4);
 
         sc.close();
     }
